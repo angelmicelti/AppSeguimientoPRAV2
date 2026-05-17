7e9b6a9f6afc2f8a586de3ebb1f11feb68df9e4f
angelmicelti <angelmicelti@users.noreply.github.com>
2026-05-17 17:19:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Gestión PRA-R
-// Versión: 1.1.0
+// Versión: 1.3.0
 
-const CACHE_NAME = 'pra-r-cache-v1.1';
+const CACHE_NAME = 'pra-r-cache-v1.3';
 
 // Archivos esenciales para cachear en la instalación
 const PRECACHE_URLS = [
